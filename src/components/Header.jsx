b0c343fdb1fd6e583bcd998164c0262b89b38655
Aaron Dalager <113053870+Ungabad@users.noreply.github.com>
2024-09-10 20:01:13 -0700

    Added some of the content needed

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Welcome to Todo App</h1>
+      <p>This is a simple Todo App using React and Vite.</p>
+      <nav>
+        <a href='https://vitejs.dev' target='_blank'>
+          Learn Vite
+        </a>
+        <a href='https://reactjs.org' target='_blank'>
+          Learn React
+        </a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/MainC.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function MainC() {
+  return (
+    <main>
+      <h1>Todo1</h1>
+      <input type='checkbox' name='' id='' />
+      <h1>Todo2</h1>
+      <input type='checkbox' name='' id='' />
+      <h1>Todo3</h1>
+      <input type='checkbox' name='' id='' />
+      <h1>Todo4</h1>
+      <input type='checkbox' name='' id='' />
+    </main>
+  );
+}
+
+export default MainC;

acfdba689bd4f3545a85aef5b9afdfa50fd71846
Aaron Dalager <113053870+Ungabad@users.noreply.github.com>
2024-10-01 18:36:20 -0700

    Fix something, now more cors

--- src/components/MainC.jsx
+++ src/components/MainC.jsx
@@ -23,7 +23,6 @@ function MainC() {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "https://unga-todo.netlify.app",
         },
       });
       const data = await response.json();
@@ -61,7 +60,6 @@ function MainC() {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "https://unga-todo.netlify.app",
         },
         body: JSON.stringify(newItem),
       });
@@ -80,7 +78,6 @@ function MainC() {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "https://unga-todo.netlify.app",
       },
     });
     setChecklistItems(updatedItems);
@@ -104,7 +101,6 @@ function MainC() {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "https://unga-todo.netlify.app",
       },
     });
     setEditItemId(null); // Exit edit mode
